58cd6a151fecd38ff49bf2dd57b41d3ca7eaa9a9
Darknroses <jad.goeppner@icloud.com>
2023-09-11 21:15:26 +0200

    Upgrade EE

--- openai_chat/models/res_users.py
+++ openai_chat/models/res_users.py
@@ -2,7 +2,7 @@
 # Copyright (C) 2023 - Myrrkel (https://github.com/myrrkel).
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api, _
+from odoo import models
 import logging
 
 _logger = logging.getLogger(__name__)
